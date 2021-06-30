7d4de2c33b422ef8c376ebdca61610587b37ef91
ratinac-nikola <ratinac.nikola@gmail.com>
2021-06-30 15:10:38 +0200

    city data preprocessing
    renamed several columns
    extracted month and year

--- DataPreparation.scala
+++ DataPreparation.scala
@@ -1,12 +1,26 @@
 
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.types.IntegerType
 object DataPreparation {
   def main(args: Array[String]) {
     val csvPath = "data/GlobalLandTemperaturesByCity.csv" 
     val spark = SparkSession.builder.appName("Data Preparation").getOrCreate()
-    val tempData = spark.read.csv(csvPath).cache()
-    tempData.show(15)
+    
+    val data = spark.read.format("csv")
+      .option("header", true)
+      .option("inferSchema", true)
+      .load(csvPath)
+
+    data
+      .withColumnRenamed("Latitude", "lat")
+      .withColumnRenamed("Longitude", "lng")
+      .withColumnRenamed("AverageTemperature", "temperature")
+      .withColumn("month", col("dt").substr(6,2).cast(IntegerType))
+      .withColumn("year", col("dt").substr(1,4).cast(IntegerType))
+      .select("temperature", "City", "Country", "lat", "lng", "year", "month")
+      .show(10)
     spark.stop()
   }
 }
+
